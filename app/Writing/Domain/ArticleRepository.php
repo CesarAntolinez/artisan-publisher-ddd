6074ecd09d07b289c5eaaa1bddc1d25b987b8265
CesarAntolinez <cesaralejandroantolinez@gmail.com>
2025-09-05 14:38:52 -0500

    crear el contrato de articulo

---
+++ app/Writing/Domain/ArticleRepository.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Writing\Domain;
+
+// Usamos un 'use' para importar nuestra entidad y que el código sea más limpio.
+use Writing\Domain\Article;
+
+interface ArticleRepository
+{
+    /**
+     * Busca un artículo por su ID.
+     * Puede devolver un Artículo o null si no lo encuentra.
+     */
+    public function findById(string $id): ?Article;
+
+    /**
+     * Guarda un artículo (ya sea nuevo o una actualización).
+     */
+    public function save(Article $article): void;
+}
